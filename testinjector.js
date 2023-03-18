70c5b5402b67661c83c3a480c988ab8da3e37382
UnluckyCrafter <117037081+minidogg@users.noreply.github.com>
2023-03-18 15:10:56 -0500

    Update testinjector.js

--- testinjector.js
+++ testinjector.js
@@ -1,22 +1,6 @@
-function getgit (owner, repo, path) { 
-    // A function to fetch files from github using the api 
-    
-  let data = await fetch (
-    `https://api.github.com/repos/${owner}/${repo}/contents/${path}`
-  )
-    .then (d => d.json ())
-    .then (d =>
-      fetch (
-        `https://api.github.com/repos/${owner}/${repo}/git/blobs/${d.sha}`
-      )
-    )
-    .then (d => d.json ())
-    .then (d => JSON.parse (atob (d.content)));
-
-  return data;
-}
-
-var style = getgit("Crafterboy27-school","Hexbois-visual-mod","/style/latest");
-let newEl = document.createElement("style");
-newEl.innerHTML = style;
-document.body.appendChild(newEl);
+(() => {
+    var styleEl = document.createElement('style');
+    styleEl.src = "https://raw.githubusercontent.com/minidogg/Hexbois-visual-mod/main/style/latest.css";
+    styleEl.id = "hostname";
+    document.body.appendChild(styleEl);
+})();
